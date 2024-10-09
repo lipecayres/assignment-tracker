88d6fb5f990c23188dc7dfcc481dc2c836846add
Felipe Cayres <felipescayres@gmail.com>
2024-10-09 11:46:22 -0400

    update font and title

--- view/header.php
+++ view/header.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Assignment Tracker</title>
+    <title>To Do List</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="view/css/main.min.css" />
 </head>
 
